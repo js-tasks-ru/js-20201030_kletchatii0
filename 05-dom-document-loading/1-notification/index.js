7b31e7954309289c244b0d4bbab6d9f438e5c238
mikenai <kletchatii@gmail.com>
2020-11-23 20:37:26 +0200

    add 3-5 tasks

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,53 @@
+const getTpl = ({message, duration = '20', type = 'success'}) => `
+ <div class="notification ${type}" style="--value:${duration}s">
+    <div class="timer"></div>
+    <div class="inner-wrapper">
+      <div class="notification-header">${type}</div>
+      <div class="notification-body">
+        ${message}
+      </div>
+    </div>
+  </div>
+`;
+
 export default class NotificationMessage {
+  /** @type {NotificationMessage} */
+  activeNotification = null;
+
+  constructor(message, options = {}) {
+    this.message = message;
+    this.duration = options.duration;
+    this.type = options.type;
+    if (NotificationMessage.activeNotification) {
+      NotificationMessage.activeNotification.remove();
+    }
+    this.render();
+  }
+
+  render() {
+    let ctnr = document.createElement('div');
+    ctnr.innerHTML = getTpl({
+      message: this.message,
+      duration: this.duration / 1000,
+      type: this.type
+    });
+    this.element = ctnr.children[0];
+  }
+
+  remove() {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.remove();
+    NotificationMessage.activeNotification = null;
+  }
+
+  show(parent = document.body) {
+    parent.append(this.element);
+    setTimeout(() => {
+      this.remove();
+    }, this.duration);
+  }
 
 }
