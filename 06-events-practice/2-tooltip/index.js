8e59adb3a899ecf817c0fea68e30a2f6e87d4ae9
mikenai <kletchatii@gmail.com>
2020-11-23 23:21:01 +0200

    add tooltip

--- 06-events-practice/2-tooltip/index.js
+++ 06-events-practice/2-tooltip/index.js
@@ -1,5 +1,49 @@
-class Tooltip {
+const getTpl = (text = '') => `<div class="tooltip">${text}</div>`;
 
+class Tooltip {
+  listenersRemovers = [];
+  render(text) {
+    const ctnr = document.createElement('div');
+    ctnr.innerHTML = getTpl(text);
+    this.element = ctnr.firstElementChild;
+  }
+
+  initialize() {
+    const onPointerOut = (event) => {
+      const tooltipTarget = event.target.closest('[data-tooltip]');
+      if (tooltipTarget)
+        this.element.remove();
+    };
+    document.body.addEventListener('pointerout', onPointerOut, true);
+    this.listenersRemovers.push(() => document.body.removeEventListener('pointerout', onPointerOut));
+
+    const onMouseMove = (event) => {
+      const tooltipTarget = event.target.closest('[data-tooltip]');
+      if (!tooltipTarget) return;
+      this.element.style.top = event.clientY + 5 + 'px';
+      this.element.style.left = event.clientX + 5 + 'px';
+    };
+    document.body.addEventListener('mousemove', onMouseMove);
+    this.listenersRemovers.push(() => document.body.removeEventListener('mousemove', onMouseMove));
+
+    const onPointerOver = (event) => {
+      const tooltipTarget = event.target.closest('[data-tooltip]');
+      if (!tooltipTarget) return;
+      this.render(tooltipTarget.dataset.tooltip);
+      document.body.append(this.element);
+    };
+    document.body.addEventListener('pointerover', onPointerOver);
+    this.listenersRemovers.push(() => document.body.removeEventListener('pointerover', onPointerOver));
+  }
+
+  remove() {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.listenersRemovers.forEach((rm) => rm());
+    this.remove();
+  }
 }
 
 const tooltip = new Tooltip();
